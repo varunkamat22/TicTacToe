f0d3246bd31a4ff9554c9f89a622b6554ea5de86
v0k009n <varun.kamat@walmartlabs.com>
2020-06-20 19:48:20 +0530

    Turned off app re orientation becuase of device rotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown,DeviceOrientation.portraitUp]);
   runApp(MyApp());
 }
 
